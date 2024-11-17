5d5c76e202d209d454d4371a1a33f0761a44ba06
Breno Cunha Queiroz <breno1423@gmail.com>
2024-11-17 07:30:52 +0100

    fix: implot3d definition

--- implot3d.cpp
+++ implot3d.cpp
@@ -27,14 +27,14 @@
 ImPlot3DContext* GImPlot3D = nullptr;
 #endif
 
-ImPlot3DContext* CreateContext() {
+ImPlot3DContext* ImPlot3D::CreateContext() {
     ImPlot3DContext* ctx = IM_NEW(ImPlot3DContext)();
     if (GImPlot3D == nullptr)
         SetCurrentContext(ctx);
     return ctx;
 }
 
-void DestroyContext(ImPlot3DContext* ctx) {
+void ImPlot3D::DestroyContext(ImPlot3DContext* ctx) {
     if (ctx == nullptr)
         ctx = GImPlot3D;
     if (GImPlot3D == ctx)
@@ -42,8 +42,8 @@ void DestroyContext(ImPlot3DContext* ctx) {
     IM_DELETE(ctx);
 }
 
-ImPlot3DContext* GetCurrentContext() { return GImPlot3D; }
+ImPlot3DContext* ImPlot3D::GetCurrentContext() { return GImPlot3D; }
 
-void SetCurrentContext(ImPlot3DContext* ctx) { GImPlot3D = ctx; }
+void ImPlot3D::SetCurrentContext(ImPlot3DContext* ctx) { GImPlot3D = ctx; }
 
 #endif // #ifndef IMGUI_DISABLE
