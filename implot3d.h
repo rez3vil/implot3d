7b1fc808983fcbc447fe8c843149034e9b2527e8
Lumengine <p.baptiste91@gmail.com>
2024-12-18 16:56:39 +0100

    Missing struct IMPLOT3D_API

--- implot3d.h
+++ implot3d.h
@@ -627,8 +627,8 @@ struct ImPlot3DRange {
     constexpr ImPlot3DRange() : Min(0.0f), Max(0.0f) {}
     constexpr ImPlot3DRange(float min, float max) : Min(min), Max(max) {}
 
-    void Expand(float value);
-    bool Contains(float value) const;
+    IMPLOT3D_API void Expand(float value);
+    IMPLOT3D_API bool Contains(float value) const;
     float Size() const { return Max - Min; }
 };
 
@@ -643,41 +643,41 @@ struct ImPlot3DQuat {
     constexpr ImPlot3DQuat() : x(0.0f), y(0.0f), z(0.0f), w(1.0f) {}
     constexpr ImPlot3DQuat(float _x, float _y, float _z, float _w) : x(_x), y(_y), z(_z), w(_w) {}
 
-    ImPlot3DQuat(float _angle, const ImPlot3DPoint& _axis);
+    IMPLOT3D_API ImPlot3DQuat(float _angle, const ImPlot3DPoint& _axis);
 
     // Set quaternion from two vectors
-    static ImPlot3DQuat FromTwoVectors(const ImPlot3DPoint& v0, const ImPlot3DPoint& v1);
+    IMPLOT3D_API static ImPlot3DQuat FromTwoVectors(const ImPlot3DPoint& v0, const ImPlot3DPoint& v1);
 
     // Get quaternion length
-    float Length() const;
+    IMPLOT3D_API float Length() const;
 
     // Get normalized quaternion
-    ImPlot3DQuat Normalized() const;
+    IMPLOT3D_API ImPlot3DQuat Normalized() const;
 
     // Conjugate of the quaternion
-    ImPlot3DQuat Conjugate() const;
+    IMPLOT3D_API ImPlot3DQuat Conjugate() const;
 
     // Inverse of the quaternion
-    ImPlot3DQuat Inverse() const;
+    IMPLOT3D_API ImPlot3DQuat Inverse() const;
 
     // Binary operators
-    ImPlot3DQuat operator*(const ImPlot3DQuat& rhs) const;
+    IMPLOT3D_API ImPlot3DQuat operator*(const ImPlot3DQuat& rhs) const;
 
     // Normalize the quaternion in place
-    ImPlot3DQuat& Normalize();
+    IMPLOT3D_API ImPlot3DQuat& Normalize();
 
     // Rotate a 3D point using the quaternion
-    ImPlot3DPoint operator*(const ImPlot3DPoint& point) const;
+    IMPLOT3D_API ImPlot3DPoint operator*(const ImPlot3DPoint& point) const;
 
     // Comparison operators
-    bool operator==(const ImPlot3DQuat& rhs) const;
-    bool operator!=(const ImPlot3DQuat& rhs) const;
+    IMPLOT3D_API bool operator==(const ImPlot3DQuat& rhs) const;
+    IMPLOT3D_API bool operator!=(const ImPlot3DQuat& rhs) const;
 
     // Interpolate between two quaternions
-    static ImPlot3DQuat Slerp(const ImPlot3DQuat& q1, const ImPlot3DQuat& q2, float t);
+    IMPLOT3D_API static ImPlot3DQuat Slerp(const ImPlot3DQuat& q1, const ImPlot3DQuat& q2, float t);
 
     // Get quaternion dot product
-    float Dot(const ImPlot3DQuat& rhs) const;
+    IMPLOT3D_API float Dot(const ImPlot3DQuat& rhs) const;
 
 #ifdef IMPLOT3D_QUAT_CLASS_EXTRA
     IMPLOT3D_QUAT_CLASS_EXTRA // Define additional constructors and implicit cast operators in imconfig.h to convert back and forth between your math types and ImPlot3DQuat
